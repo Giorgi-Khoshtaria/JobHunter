5d1caa0e21d8f88f067c6da863b82e33de56de57
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2025-07-31 13:46:38 +0400

    feat: add header esponsive

---
+++ Front-jobHunter/src/Components/Header/Header.tsx
@@ -0,0 +1,155 @@
+import React, { useState, useEffect, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import logo from "../../assets/jobHunter-logo.png";
+
+function Header() {
+  const [open, setOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+
+  useEffect(() => {
+    const handleDropDown = (event: MouseEvent) => {
+      const target = event.target as HTMLElement;
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(target) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(target)
+      ) {
+        setOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleDropDown);
+    return () => {
+      document.removeEventListener("mousedown", handleDropDown);
+    };
+  });
+
+  const navArray = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/about" },
+    { name: "Contact", path: "/contact" },
+  ];
+
+  return (
+    <header>
+      <div className="flex items-center justify-between bg-indigo p-4">
+        <div>
+          <a href="/">
+            <img src={logo} alt="logo" className="w-32 h-12 object-cover" />
+          </a>
+        </div>
+
+        <nav className="max-sm:hidden">
+          <ul className="flex gap-6">
+            {navArray.map((item) => (
+              <li key={item.name} className="text-white font-semibold">
+                <a
+                  href={item.path}
+                  className="hover:underline text-white font-bold"
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+
+        <div className="relative max-sm:hidden">
+          <button
+            onClick={() => setOpen(!open)}
+            ref={buttonRef}
+            className="cursor-pointer ml-6 px-4 py-2 text-white rounded hover:bg-indigo-700 transition"
+          >
+            For Employers
+          </button>
+        </div>
+
+        <div className="hidden max-sm:flex">
+          <button onClick={() => setIsMobile(true)}>
+            <FontAwesomeIcon
+              icon={faBars}
+              size="xl"
+              style={{ color: "#ffffff" }}
+            />
+          </button>
+        </div>
+      </div>
+
+      {open && (
+        <div
+          ref={dropdownRef}
+          className="absolute right-5 mt-2 w-48 bg-white border rounded shadow-lg"
+        >
+          <a
+            href="/employer/login"
+            className="block px-4 py-2 hover:bg-gray-100"
+          >
+            Company Login
+          </a>
+          <a
+            href="/employer/register"
+            className="block px-4 py-2 hover:bg-gray-100"
+          >
+            Register Company
+          </a>
+        </div>
+      )}
+
+      <div
+        className={`fixed inset-0 bg-black/50 z-20 transition-opacity duration-300 ${
+          isMobile ? "opacity-100" : "opacity-0 pointer-events-none"
+        }`}
+        onClick={() => setIsMobile(false)}
+      ></div>
+
+      <div
+        className={`fixed top-0 right-0 w-64 h-full bg-indigo z-30 p-6 transform transition-transform duration-300 ${
+          isMobile ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <button
+          className="absolute right-3 top-4"
+          onClick={() => setIsMobile(false)}
+        >
+          <FontAwesomeIcon
+            icon={faXmark}
+            size="xl"
+            style={{ color: "#ffffff" }}
+          />
+        </button>
+
+        <nav className="mt-10">
+          <ul className="flex flex-col gap-6">
+            {navArray.map((item) => (
+              <li key={item.name} className="text-white font-semibold">
+                <a
+                  href={item.path}
+                  className="hover:underline text-white font-bold"
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
+            <a
+              href="/employer/register"
+              className="block text-white font-semibold"
+            >
+              Register Company
+            </a>
+            <a
+              href="/employer/login"
+              className="block text-white font-semibold"
+            >
+              Company Login
+            </a>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
