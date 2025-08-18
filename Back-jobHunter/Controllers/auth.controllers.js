f7c52fae36160caebc734e68c3f48d25a9f25247
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2025-08-18 16:11:51 +0400

    add forgot password functionality

--- Back-jobHunter/Controllers/auth.controllers.js
+++ Back-jobHunter/Controllers/auth.controllers.js
@@ -86,3 +86,28 @@ export const login = async (req, res, next) => {
     next(error);
   }
 };
+
+export const forgotPassword = async (req, res, next) => {
+  const { email, newPassword, confirmPassword } = req.body;
+  if (!email || !newPassword || !confirmPassword) {
+    return res.status(400).json({ message: "All fields are required." });
+  }
+
+  if (newPassword !== confirmPassword) {
+    return res.status(400).json({ message: "Passwords do not match." });
+  }
+
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found." });
+    }
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    user.password = hashedPassword;
+    await user.save();
+
+    res.status(200).json({ message: "Password updated successfully." });
+  } catch (error) {
+    res.status(500).json({ message: "Server error. Please try again later." });
+  }
+};
--- Back-jobHunter/Routes/auth.route.js
+++ Back-jobHunter/Routes/auth.route.js
@@ -1,9 +1,14 @@
 import express from "express";
 const authRouter = express.Router();
 
-import { login, signUp } from "../Controllers/auth.controllers.js";
+import {
+  forgotPassword,
+  login,
+  signUp,
+} from "../Controllers/auth.controllers.js";
 
 authRouter.post("/signUp", signUp);
 authRouter.post("/login", login);
+authRouter.put("/reset-password", forgotPassword);
 
 export default authRouter;
